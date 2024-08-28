5753515e8590a157e90f82ee27fec6ad0df9e205
Wasprogramador <00001114060525sp@al.educacao.sp.gov.br>
2024-08-28 20:30:01 -0300

    exercicio

---
+++ aula 03/exercicio/exercicio.py
@@ -0,0 +1,20 @@
+# lista de alimentos que pedro fez no verdurão
+alimentos = [
+    "abacaxi",
+    "maçã",
+    "banana",
+    "alface",
+    "kiwi",
+    "alho"
+    "cebola",
+    "gengibre",
+    "beterraba",
+    "batata doce",
+    "ervilha"
+]
+alimentos.sort()
+
+y = 1
+for x in alimentos:
+    print(y,"-",x)
+    y=y+1
---
+++ aula 03/exercicio/exericio 2.py
@@ -0,0 +1,22 @@
+# lista de alimentos que pedro fez no verdurão
+x = [
+    "abacaxi",
+    "maçã",
+    "banana",
+    "alface",
+    "kiwi",
+    "alho"
+    "cebola",
+    "gengibre",
+    "beterraba",
+    "batata doce",
+    "ervilha"
+]
+x = 10
+while (x>=0):
+    print(x)
+    x = x-1
+    break
+#aviso ta repetindo infinitamente
+    
+   
