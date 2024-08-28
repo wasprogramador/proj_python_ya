aabd3a15ed5395ef6eaf141fa6447828a3ec6d64
Wasprogramador <00001114060525sp@al.educacao.sp.gov.br>
2024-08-28 18:56:09 -0300

    laço de repetiao

---
+++ aula 03/para 2.py
@@ -0,0 +1,19 @@
+alunos = [
+    "gustavo",
+    "Joao",
+    "Matheus",
+    "Rebeca",
+    "Vitor",
+    "Claudio",
+    "Geraldo",
+    "Rodrigo",
+    "Bianca",
+    "Gabriela"
+]
+alunos.sort()
+alunos.append("Henrrique")
+alunos.reverse()
+y = 1
+for x in alunos:
+    print(y,"-",x)
+    y=y+1
