3cbba7ffd279797341790d8a8ae32a42ff0c77e7
Wasprogramador <00001114060525sp@al.educacao.sp.gov.br>
2024-08-28 17:41:40 -0300

    aula de revisao

---
+++ revisoes/revisao 2.py
@@ -0,0 +1,6 @@
+idade = 16
+if (idade >=18):
+    print("maior de idade")
+else:
+    print("menor de idade")
+    
---
+++ revisoes/revisao 3.py
@@ -0,0 +1,7 @@
+idade = 16
+if (idade >=18):
+    print("adulto")
+elif (idade >=13):
+    print("adolecente")
+else:
+    print("criança")
