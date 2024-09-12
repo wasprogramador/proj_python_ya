95706f88a5d68eb8f662cecacb0256ba3a81c320
Wasprogramador <00001114060525sp@al.educacao.sp.gov.br>
2024-09-11 21:04:39 -0300

    list map e list comprehesion

--- aula05/Conversao.py
+++ aula05/Conversao.py
@@ -2,20 +2,23 @@ class Conversao:
     def velocidade():
         kmh = [75.4,30.6,120,32.8,20.8]
         mph = [round(x/1.61,2) for x in kmh]
-        return mph 
+        x = 0
+        while(x<5):
+            print(kmh[x],"kmh são", mph[x])
+            x += 1 
     
     def temperatura():
         celsius = [45,30,12.5,32.6,50]
         fahrenheit = [round((x*1.8)+32,2) for x in celsius]
         x = 0
-        while(x<6):
+        while(x<5):
             print(celsius[x],"celsius são", fahrenheit[x])
             x += 1 
     def altura():
         metros = [5,10,20,35,70]
         pes = [round(x/0.3048,2) for x in metros]
         x = 0
-        while(x<4):
+        while(x<5):
             print(metros[x],"em pés são", pes[x])
             x += 1
     
